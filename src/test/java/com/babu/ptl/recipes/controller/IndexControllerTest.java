0185bcd1141c0148cb7e5d219f28231bca3352c0
`navneet <navneet.gs@gmail.com>
2022-02-11 06:00:12 +0530

    Checked Set recipes with argument captor

--- src/test/java/com/babu/ptl/recipes/controller/IndexControllerTest.java
+++ src/test/java/com/babu/ptl/recipes/controller/IndexControllerTest.java
@@ -1,11 +1,16 @@
 package com.babu.ptl.recipes.controller;
 
+import com.babu.ptl.recipes.domain.Recipe;
 import com.babu.ptl.recipes.service.recipeservice.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -27,10 +32,24 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipeSet.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         String viewName = indexController.getIndexPage(model);
 
         assertEquals("index", viewName);
         verify(recipeService,times(1) ).getRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model,times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2,setInController.size());
     }
 }
