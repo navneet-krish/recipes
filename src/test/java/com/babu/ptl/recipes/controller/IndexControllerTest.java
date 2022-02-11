ba1ea8edde45ef5c055b267299d1bbce4bd26dcc
`navneet <navneet.gs@gmail.com>
2022-02-11 05:39:08 +0530

    Controller unit test

---
+++ src/test/java/com/babu/ptl/recipes/controller/IndexControllerTest.java
@@ -0,0 +1,36 @@
+package com.babu.ptl.recipes.controller;
+
+import com.babu.ptl.recipes.service.recipeservice.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+    @Mock
+    Model model;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        String viewName = indexController.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService,times(1) ).getRecipes();
+        verify(model,times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
