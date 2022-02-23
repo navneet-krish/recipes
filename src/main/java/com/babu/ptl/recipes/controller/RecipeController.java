53805f1bbb0330cf7ed39825ce432cf191385318
`navneet <navneet.gs@gmail.com>
2022-02-23 06:26:41 +0530

    Updating recipe in progress....more work required.

--- src/main/java/com/babu/ptl/recipes/controller/RecipeController.java
+++ src/main/java/com/babu/ptl/recipes/controller/RecipeController.java
@@ -1,9 +1,12 @@
 package com.babu.ptl.recipes.controller;
 
+import com.babu.ptl.recipes.commands.RecipeCommand;
 import com.babu.ptl.recipes.service.recipeservice.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,12 +18,27 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show/")
     public String showById(@PathVariable String id, Model model){
 
              model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:show/" + savedCommand.getId();
     }
 
 }
