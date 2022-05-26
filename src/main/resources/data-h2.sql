c18886faae51400b7c59238de24c705ae927f984
`navneet <navneet.gs@gmail.com>
2022-05-26 05:55:15 +0530

    finally ran application with mysql local instance

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,9 @@
+insert into category (description) values ('American');
+insert into category (description) values ('Mexican');
+insert into category (description) values ('Punjabi');
+insert into category (description) values ('Indian');
+insert into unit_of_measure (description) values ('Swad Anusar');
+insert into unit_of_measure (description) values ('Pinch');
+insert into unit_of_measure (description) values ('Cup');
+insert into unit_of_measure (description) values ('Tablespoon');
+insert into unit_of_measure (description) values ('Teaspoon');
--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-insert into Category (description) values ('American');
-insert into Category (description) values ('Mexican');
-insert into Category (description) values ('Punjabi');
-insert into Category (description) values ('Indian');
-insert into Unit_Of_Measure (description) values ('Swad Anusar');
-insert into Unit_Of_Measure (description) values ('Pinch');
-insert into unit_of_measure (description) values ('Cup');
-insert into unit_of_measure (description) values ('Tablespoon');
-insert into unit_of_measure (description) values ('Teaspoon');
